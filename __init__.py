9a8e4457078a66db1b0b20687fa4a37764dfd4e3
Justin Moon <jmoon@lividinstruments.com>
2025-09-11 22:42:30 -0400

    setting up new project

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from .simple_fcb1010 import simple_fcb1010
+from .fcb1010_historical_looper import fcb1010_historical_looper
 
 def create_instance(c_instance):
-    return simple_fcb1010(c_instance)
+    return fcb1010_historical_looper(c_instance)
